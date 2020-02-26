c4fd37a441b99ca889a2f322726a3dba13b15ee0
okafor obinna <kingsloob1@gmail.com>
2020-02-26 12:59:32 +0100

    make chart component the default coponent

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <chart-component />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import ChartComponent from "./components/ChartComponent.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    ChartComponent
   }
 };
 </script>
 
-<style>
+<style lang="less" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  border: 1px solid rgb(180, 180, 180);
+  box-shadow: 2px 2px 4px rgb(59, 59, 59);
+  width: 100%;
+  height: 100vh;
+
+  @media only screen and (min-width: 450px) {
+    width: 90%;
+    margin-right: auto;
+    margin-left: auto;
+  }
+
+  @media only screen and (min-width: 760px) {
+    width: 80%;
+  }
+
+  @media only screen and (min-width: 1024px) {
+    width: 70%;
+  }
 }
 </style>
