019f16611770ac3f63052005bc692e8de5fee735
okafor obinna <kingsloob1@gmail.com>
2020-02-26 12:54:46 +0100

    Added inline error component

---
+++ src/components/ErrorInner.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="error">
+    <div class="image">
+      <img v-show="showImageElement" :src="imageSrc" />
+    </div>
+    <div class="text">{{ errorText }}</div>
+  </div>
+</template>
+
+<script>
+import img from "../assets/images/error.png";
+
+export default {
+  props: {
+    /** loader text  */
+    text: {
+      type: String,
+      required: true
+    },
+
+    /** option to show loader image  */
+    showImage: {
+      type: Boolean,
+      required: false,
+      default: true
+    },
+
+    /** option to change loader image  */
+    imageUrl: {
+      type: String,
+      required: false,
+      default: img
+    }
+  },
+  computed: {
+    imageSrc() {
+      return this.imageUrl;
+    },
+    errorText() {
+      return this.text;
+    },
+    showImageElement() {
+      return this.showImage;
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.error {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+
+  > .image {
+    &:extend(.error);
+    height: auto;
+    margin-bottom: 10px;
+    position: relative;
+    z-index: 1;
+
+    > img {
+      width: 70px;
+      height: 70px;
+      display: flex;
+    }
+  }
+
+  > .text {
+    &:extend(.error);
+    height: auto;
+    margin-top: 10px;
+    font-size: 15px;
+    text-transform: capitalize;
+    font-weight: 700;
+    color: rgb(95, 0, 0);
+  }
+}
+</style>
